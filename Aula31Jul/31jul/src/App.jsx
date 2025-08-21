644ada9012a5b247778ac063453f67adec026fd3
dourado840 <fdouradodeczka@gmail.com>
2025-08-20 22:07:32 -0300

    Atualizacao

---
+++ Aula31Jul/31jul/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from "react"
+import './App.css'
+import Header from "./components/header/Index"
+
+
+function App() {
+
+  return (
+    <>
+        <Header titulo="Meu Header"/>
+    </>
+  )
+}
+
+export default App
